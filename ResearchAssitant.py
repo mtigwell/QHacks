4104e217627b0fe6349ed11ba8b9460313ff8d13
mtigwell <14mbt4@queensu.ca>
2019-02-02 16:23:15 -0500

    Merge branch 'master' of https://github.com/mtigwell/QHacks
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- ResearchAssitant.py
+++ ResearchAssitant.py
@@ -8,11 +8,7 @@ import requests
 indicoio.config.api_key = '10b9bc05e39205de419a80cc3263ea3c'
 
 def getWords(url):
-    # try:
-    # uClient= uReq(url)
-    # page_html = uClient.read()
     page_html = requests.get(url)
-    # uClient.close()
     page_soup = Soup(page_html.text, "html.parser")
     paragraphs = page_soup.findAll('p')
     data = ""
@@ -29,37 +25,11 @@ def getWords(url):
             data = data + " " + text[len(text)-1]
     return data
 
-    # except Exception:
-    #     print("oops")
-
-
-def SummerizeWebsite(url):
-    string= indicoio.summarization(url)
-    data = ""
-    for i in range(len(string)):
-        print(i)
-        s = string[i]
-        s = s.strip('\n')
-        s = re.sub(r'\[.*?\]', '', s)
-        print(string)
-        data = data + " " + s
-    return data
-
 
 def SummerizeText(text):
     t = summarizer.summarize(text)
-    t= re.sub(r'\[.*?\]','', t)
-    t= t.strip('\n')
-    t= t.strip('\t')
-    print(t)
+    t = re.sub(r'\[.*?\]','', t)
+    t = t.strip('\n')
+    t = t.strip('\t')
     return t
-
-# url = "http://blog.christianperone.com/2013/09/machine-learning-cosine-similarity-for-vector-space-models-part-iii/"
-# # url = "https://en.wikipedia.org/wiki/Alexander_the_Great"
-# word = getWords(url)
-# SummerizeText(word)
-# print()
-# print()
-# word = SummerizeWebsite(url)
-# print(word)
 
